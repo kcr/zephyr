030d947f32e95cdb3c51a538a9ee72419216a27a
Craig Fields <cfields@mit.edu>
1990-04-04 19:14:22 +0000

    Initial revision

---
+++ prototype.c
@@ -0,0 +1,20 @@
+You should replace all of the <> bracketed text before you can compile
+this file.
+/* This file is part of the Project Athena Zephyr Notification System.
+ * It contains functions for <frobbing nakrabs>.
+ *
+ *	Created by:	<Joe Random Hacker>
+ *
+ *	$Source: /srv/kcr/athena/zephyr/Attic/prototype.c,v $
+ *	$Author: cfields $
+ *
+ *	Copyright (c) 1990 by the Massachusetts Institute of Technology.
+ *	For copying and distribution information, see the file
+ *	"mit-copyright.h". 
+ */
+
+#include <zephyr/mit-copyright.h>
+
+#ifndef lint
+static char rcsid_<foo_c>[] = "$Header: /srv/kcr/athena/zephyr/Attic/prototype.c,v 1.1 1990-04-04 19:14:22 cfields Exp $";
+#endif lint
