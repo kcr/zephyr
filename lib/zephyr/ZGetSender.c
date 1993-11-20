bb2dd26c3e78c9a734d9d4d866ede642618d8854
Richard Basch <probe@mit.edu>
1993-11-20 17:18:13 +0000

    Removed getuid() decl (leave it to the system include files)

--- lib/zephyr/ZGetSender.c
+++ lib/zephyr/ZGetSender.c
@@ -10,7 +10,7 @@
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /srv/kcr/locker/zephyr/lib/zephyr/ZGetSender.c,v 1.9 1993-11-21 03:11:08 probe Exp $ */
+/* $Header: /srv/kcr/locker/zephyr/lib/zephyr/ZGetSender.c,v 1.10 1993-11-21 03:18:13 probe Exp $ */
 
 #ifndef lint
 static char rcsid_ZGetSender_c[] =
@@ -21,8 +21,6 @@ static char rcsid_ZGetSender_c[] =
 #include <zephyr/zephyr_internal.h>
 
 #include <pwd.h>
-
-uid_t getuid();
 
 char *ZGetSender()
 {
