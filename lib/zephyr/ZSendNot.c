feec69b97f10bb7807f20de04564b6cc711758ae
Greg Hudson <ghudson@mit.edu>
1994-11-01 07:51:30 +0000

    Changes for better authentication.

--- lib/zephyr/ZSendNot.c
+++ lib/zephyr/ZSendNot.c
@@ -4,16 +4,16 @@
  *	Created by:	Robert French
  *
  *	$Source: /srv/kcr/locker/zephyr/lib/zephyr/ZSendNot.c,v $
- *	$Author: lwvanels $
+ *	$Author: ghudson $
  *
  *	Copyright (c) 1987 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /srv/kcr/locker/zephyr/lib/zephyr/ZSendNot.c,v 1.10 1991-12-04 13:48:18 lwvanels Exp $ */
+/* $Header: /srv/kcr/locker/zephyr/lib/zephyr/ZSendNot.c,v 1.11 1994-11-01 17:51:59 ghudson Exp $ */
 
 #ifndef lint
-static char rcsid_ZSendNotice_c[] = "$Header: /srv/kcr/locker/zephyr/lib/zephyr/ZSendNot.c,v 1.10 1991-12-04 13:48:18 lwvanels Exp $";
+static char rcsid_ZSendNotice_c[] = "$Header: /srv/kcr/locker/zephyr/lib/zephyr/ZSendNot.c,v 1.11 1994-11-01 17:51:59 ghudson Exp $";
 #endif
 
 #include <zephyr/mit-copyright.h>
@@ -44,7 +44,8 @@ Code_t ZSrvSendNotice(notice, cert_routine, send_routine)
     if ((retval = ZParseNotice(buffer, len, &newnotice)) != ZERR_NONE)
 	return (retval);
     
-    retval = Z_SendFragmentedNotice(&newnotice, len, send_routine);
+    retval = Z_SendFragmentedNotice(&newnotice, len, cert_routine,
+				    send_routine);
 
     free(buffer);
 
