2d5f5e11be9f61e0a1f8af5fe43b649a95137d59
Greg Hudson <ghudson@mit.edu>
1996-04-04 14:30:34 +0000

    On an unhide, get the exposure level from .zephyr.vars, like zwgc does, rather than assuming EXPOSE_RLMVIS.

--- clients/zctl/zctl.c
+++ clients/zctl/zctl.c
@@ -18,7 +18,7 @@
 #include <pwd.h>
 #include <netdb.h>
 #ifndef lint
-static const char *rcsid_zctl_c = "$Id: zctl.c,v 1.27 1996-04-03 22:48:49 ghudson Exp $";
+static const char *rcsid_zctl_c = "$Id: zctl.c,v 1.28 1996-04-05 00:30:34 ghudson Exp $";
 #endif
 
 #define SUBSATONCE 7
@@ -131,7 +131,7 @@ main(argc,argv)
 		exit((code != 0));
 	} 
 
-	printf("ZCTL $Revision: 1.27 $ (Protocol %s%d.%d) - Type '?' for a list of commands.\n\n",
+	printf("ZCTL $Revision: 1.28 $ (Protocol %s%d.%d) - Type '?' for a list of commands.\n\n",
 	       ZVERSIONHDR,
 	       ZVERSIONMAJOR,ZVERSIONMINOR);
 	
@@ -376,10 +376,15 @@ do_hide(argc,argv)
 		fprintf(stderr, "Usage: %s\n",argv[0]);
 		return;
 	}
-	if (!strcmp(argv[0],"unhide"))
-		exp_level = EXPOSE_REALMVIS;
-	else
+	if (!strcmp(argv[0],"unhide")) {
+		exp_level = ZGetVariable("exposure");
+		if (exp_level)
+		    exp_level = ZParseExposureLevel(exp_level);
+		if (!exp_level)
+		    exp_level = EXPOSE_NONE;
+	} else {
 		exp_level = EXPOSE_OPSTAFF;
+	}
 	if ((retval = ZSetLocation(exp_level)) != ZERR_NONE)
 		ss_perror(sci_idx,retval,"while changing exposure status");
 	return;
