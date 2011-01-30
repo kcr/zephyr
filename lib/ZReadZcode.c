bc43e614630c3f19b22b08ee4d2b5f969d16affa
Karl Ramm <kcr@1ts.org>
2011-01-30 00:25:02 -0500

    ZReadZcodeAddr (+ make ZReadZcode take a const buffer)

--- lib/ZReadZcode.c
+++ lib/ZReadZcode.c
@@ -19,7 +19,7 @@ static const char rcsid_ZReadZcode_c[] = "$Id$";
 
 
 Code_t
-ZReadZcode(unsigned char *ptr,
+ZReadZcode(const unsigned char *ptr,
 	   unsigned char *field,
 	   int max,
 	   int *len)
@@ -45,4 +45,37 @@ ZReadZcode(unsigned char *ptr,
         return (ZERR_BADFIELD);
     *len = n;
     return (ZERR_NONE);
+}
+
+Code_t
+ZReadZcodeAddr(const unsigned char *str,
+               struct sockaddr_storage *addr)
+{
+    unsigned char addrbuf[sizeof(struct sockaddr_storage)];
+    int len;
+    int ret;
+
+    ret = ZReadZcode(str, addrbuf,
+                     sizeof(addrbuf), &len);
+    if (ret != ZERR_NONE)
+        return ret;
+
+    if (len == sizeof(struct in6_addr)) {
+        struct sockaddr_in6 *a6 = (struct sockaddr_in6 *)addr;
+        a6->sin6_family = AF_INET6;
+        memcpy(&a6->sin6_addr, addrbuf, len);
+#ifdef HAvE_SOCKADDR_IN6_SIN6_LEN
+        a6->sin6_len = sizeof(*a6);
+#endif
+    } else if (len == sizeof(struct in_addr)) {
+        struct sockaddr_in *a4 = (struct sockaddr_in *)addr;
+        a4->sin_family = AF_INET;
+        memcpy(&a4->sin_addr, addrbuf, len);
+#ifdef HAvE_SOCKADDR_IN6_SIN6_LEN
+        a4->sin6_len = sizeof(*a4);
+#endif
+    } else
+        return ZERR_BADFIELD;
+
+    return ZERR_NONE;
 }
